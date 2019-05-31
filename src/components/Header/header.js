777187e553a3fdffdf13e2b4c7b1e45bd8f5d4b2
mariapsz <mariapszonka999@gmail.com>
2019-06-01 00:16:43 +0200

    part 3 time:12.

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import style from './header.css';
-import { Link } from 'react-router-dom';
-import logo from '../../images/logo.png../../images/logo.png';
+import {Link} from 'react-router-dom';
+import image from '../../images/dog.png'
+import FontAwesome from 'react-fontawesome';
+import SideNav from "./SideNav/sideNav";
 
-const Header = () => {
+const Header = (props) => {
 
-    const logo = () => {
-        return (
-            <Link to="/" className={style.logo}>
-                <img src='' alt='nba logo'/>
-            </Link>
-        )
-    };
+    const navBars = () => (
+        <div>
+            <FontAwesome name="bars"
+                         onClick={props.onOpenNav}
+                         style={{
+                             color: '#dfdfdf',
+                             padding: '10px',
+                             cursor: 'pointer',
+                         }}
+            />
+        </div>
+    );
+
+
+    const logo = () => (
+        <Link to="/" className={style.logo}>
+            <img src={image} alt='nba logo'/>
+        </Link>
+    );
+
 
     return (
         <header className={style.header}>
+            <SideNav {...props}/>
             <div className={style.headerOpt}>
+                {navBars()}
                 {logo()}
             </div>
         </header>
