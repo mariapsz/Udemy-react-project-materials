21ad911f3841e67ef57684509ea681b2182686b0
mariapsz <mariapszonka999@gmail.com>
2019-06-04 13:32:45 +0200

    video part 13 finished

--- src/components/widgets/VideosList/videosList.js
+++ src/components/widgets/VideosList/videosList.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import styles from './viedosList.css';
 import Button from '../Buttons/button';
+import axios from 'axios';
+import {URL} from '../../../config';
+import VideosListTemplate from './videosListTemplate';
 
 class VideosList extends React.Component {
     state = {
@@ -11,11 +14,34 @@ class VideosList extends React.Component {
         amount: this.props.amount,
     };
 
+    componentWillMount() {
+        this.request(this.state.start, this.state.end)
+    }
+
+    request = (start, end) => {
+        if (this.state.teams.length < 1) {
+            axios.get(`${URL}/teams`)
+                .then(response => {
+                    this.setState({
+                        teams: response.data
+                    })
+                })
+        }
+
+        axios.get(`${URL}/videos?start=${start}&_end=${end}`)
+            .then(response => {
+                this.setState({
+                    videos: [...this.state.videos, ...response.data]
+                })
+            })
+    };
+
     renderTitle = () => {
         return this.props.title ?
             <h3><strong>NBA</strong> Videos</h3>
             : null
     };
+
 
     loadMore = () => {
 
@@ -32,10 +58,23 @@ class VideosList extends React.Component {
             <Button type='linkTo' cta='More videos' linkTo='/videos'/>
     };
 
+    renderVideos = () => {
+        let template = null;
+        switch (this.props.type) {
+            case('card'):
+                template = <VideosListTemplate data={this.state.videos} teams={this.state.teams}/>;
+                break;
+            default:
+                template = null;
+        }
+        return template;
+    };
+
     render() {
         return (
             <div className={styles.videosList_wrapper}>
                 {this.renderTitle()}
+                {this.renderVideos()}
                 {this.renderButton()}
             </div>
         )
---
+++ src/components/widgets/VideosList/videosListTemplate.js
@@ -0,0 +1,26 @@
+import styles from './viedosList.css';
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const VideosListTemplate = (props) => {
+    return props.data.map((item, i) => (
+        <Link to={`/videos/${item.id}`} key={i}>
+            <div className={styles.videoListItem_wrapper}>
+                <div className={styles.left}
+                     style={{background:'red'}}
+                >
+                    <div>
+
+                    </div>
+                </div>
+                <div className={styles.right}>
+                    <h2>
+                        {item.title}
+                    </h2>
+                </div>
+            </div>
+        </Link>
+    ))
+};
+
+export default VideosListTemplate;
